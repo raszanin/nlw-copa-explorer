7fda9e27db6970ae418155adadd5715647e81a14
Roberto Zanin <raszanin@gmail.com>
2022-11-22 13:07:14 -0300

    Result Denmark x Tunisia

--- src/script.js
+++ src/script.js
@@ -66,7 +66,7 @@ document.querySelector("#cards").innerHTML =
     "22/11",
     "terça",
     createGame("Grupo C", "argentina", "saudi-arabia", "07:00", 1, 2) +
-      createGame("Grupo D", "denmark", "tunisia", "10:00") +
+      createGame("Grupo D", "denmark", "tunisia", "10:00", 0, 0) +
       createGame("Grupo C", "mexico", "poland", "13:00") +
       createGame("Grupo D", "france", "australia", "16:00")
   ) +
